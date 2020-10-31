91e027e7f7707a6ff67212959e003c9a1ed1d155
J0NX2 <47576527+J0NX2@users.noreply.github.com>
2020-10-31 11:39:56 -0700

    Update sym.java

--- Toy/bin/syntax/sym.java
+++ Toy/bin/syntax/sym.java
@@ -9,14 +9,12 @@ package syntax;
 public class sym {
   /* terminals */
   public static final int LEFTBRACKET = 43;
+  public static final int DIVIDE = 56;
   public static final int MULT = 25;
-  public static final int UMINUS = 53;
   public static final int PERIOD = 40;
   public static final int RIGHTBRACKET = 44;
   public static final int RIGHTPAREN = 42;
   public static final int DOUBLE = 5;
-  public static final int LPAREN = 55;
-  public static final int SEMI = 54;
   public static final int GREATER = 30;
   public static final int INTERFACE = 12;
   public static final int INT = 11;
@@ -24,46 +22,50 @@ public class sym {
   public static final int LESS = 28;
   public static final int FOR = 8;
   public static final int MINUS = 24;
-  public static final int STRINGCONST = 49;
   public static final int LEFTPAREN = 41;
-  public static final int RPAREN = 56;
   public static final int NOT = 36;
   public static final int AND = 34;
-  public static final int INTCONST = 47;
   public static final int OR = 35;
   public static final int COMMA = 39;
-  public static final int DOUBLECONST = 48;
+  public static final int DOUBLECONSTANT = 48;
   public static final int SEMICOL = 38;
   public static final int CLASS = 4;
   public static final int DIV = 26;
   public static final int NEWARRAY = 14;
   public static final int PLUS = 23;
+  public static final int ASSIGN = 57;
   public static final int LESSEQ = 29;
   public static final int IF = 9;
   public static final int THIS = 20;
   public static final int ID = 51;
+  public static final int INTCONSTANT = 47;
+  public static final int LEFT = 52;
   public static final int EOF = 0;
   public static final int BOOLEAN = 2;
   public static final int RETURN = 18;
   public static final int EQUAL = 32;
+  public static final int STRINGCONSTANT = 49;
   public static final int NEW = 13;
   public static final int error = 1;
+  public static final int NEWLINE = 54;
   public static final int NULL = 15;
-  public static final int NUMBER = 57;
+  public static final int NUMBER = 58;
   public static final int MOD = 27;
+  public static final int IDENT = 59;
   public static final int BREAK = 3;
   public static final int VOID = 21;
   public static final int READLN = 17;
   public static final int ASSIGNOP = 37;
-  public static final int TIMES = 52;
+  public static final int TIMES = 55;
   public static final int RIGHTBRACE = 46;
   public static final int ELSE = 6;
-  public static final int BOOLEANCONST = 50;
   public static final int WHILE = 22;
+  public static final int BOOLEANCONSTANT = 50;
   public static final int LEFTBRACE = 45;
   public static final int EXTENDS = 7;
   public static final int GREATEREQ = 31;
   public static final int STRING = 19;
+  public static final int RIGHT = 53;
   public static final int IMPLEMENTS = 10;
   public static final int NOTEQ = 33;
   public static final String[] terminalNames = new String[] {
@@ -114,17 +116,18 @@ public class sym {
   "RIGHTBRACKET",
   "LEFTBRACE",
   "RIGHTBRACE",
-  "INTCONST",
-  "DOUBLECONST",
-  "STRINGCONST",
-  "BOOLEANCONST",
+  "INTCONSTANT",
+  "DOUBLECONSTANT",
+  "STRINGCONSTANT",
+  "BOOLEANCONSTANT",
   "ID",
+  "LEFT",
+  "RIGHT",
+  "NEWLINE",
   "TIMES",
-  "UMINUS",
-  "SEMI",
-  "LPAREN",
-  "RPAREN",
-  "NUMBER"
+  "DIVIDE",
+  "ASSIGN",
+  "NUMBER",
+  "IDENT"
   };
 }
-
